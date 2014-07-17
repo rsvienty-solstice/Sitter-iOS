32b51ecdf2d82ac9eb4f71b7751551bd34f1f443
David Maslowski <davidmaslowski@David-Maslowskis-MacBook-Pro.local>
2014-07-17 17:38:30 -0500

    IntialCommit

---
+++ Sitter-os/Sitter-os/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sitter-os
+//
+//  Created by David Maslowski on 7/17/14.
+//  Copyright (c) 2014 David Maslowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Sitter-os/Sitter-os/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Sitter-os
+//
+//  Created by David Maslowski on 7/17/14.
+//  Copyright (c) 2014 David Maslowski. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+            
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
