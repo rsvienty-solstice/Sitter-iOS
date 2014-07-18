d5b494b5328e256f01c50161def5d1610b2313b1
Jillian Offutt <offuttjillian@gmail.com>
2014-07-17 22:42:31 -0500

    Initial design implemented

--- Sitter-iOS/Sitter-iOS/ViewController.swift
+++ Sitter-iOS/Sitter-iOS/ViewController.swift
@@ -7,12 +7,34 @@
 //
 
 import UIKit
+import QuartzCore
 
 class ViewController: UIViewController {
                             
+    @IBOutlet var firstBubble: UIButton
+    @IBOutlet var secondBubble: UIButton
+    @IBOutlet var secondWhiteView: UIView
+    @IBOutlet var secondStatusView: UIView
+    @IBOutlet var thirdBubble: UIButton
+    @IBOutlet var secondNameView: UIView
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        firstBubble.layer.cornerRadius = 50;
+        secondBubble.layer.cornerRadius = 65;
+        thirdBubble.layer.cornerRadius = 80;
+        firstBubble.clipsToBounds = true
+        secondBubble.clipsToBounds = true
+        thirdBubble.clipsToBounds = true
+        
+        secondWhiteView.clipsToBounds = true
+        secondWhiteView.layer.cornerRadius = 69
+        secondStatusView.clipsToBounds = true
+        secondStatusView.layer.cornerRadius = 72
+        secondNameView.clipsToBounds = true
+        secondNameView.layer.cornerRadius = 35
     }
 
     override func didReceiveMemoryWarning() {
