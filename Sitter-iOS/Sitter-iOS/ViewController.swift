d11525d46d25982524146e30cdf7f3d8268ccf4d
Jillian Offutt <offuttjillian@gmail.com>
2014-07-17 21:02:22 -0500

    Project set up, includes a round button featuring baby Oscar!

---
+++ Sitter-iOS/Sitter-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sitter-iOS
+//
+//  Created by Jillian Offutt on 7/17/14.
+//  Copyright (c) 2014 Solstice Mobile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Sitter-iOS/Sitter-iOSTests/Sitter_iOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  Sitter_iOSTests.swift
+//  Sitter-iOSTests
+//
+//  Created by Jillian Offutt on 7/17/14.
+//  Copyright (c) 2014 Solstice Mobile. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Sitter_iOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
